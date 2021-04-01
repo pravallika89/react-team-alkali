1e07f941274ca5592a4aaf94d9669e2a517ec412
Pardeep Sanghera <pradeepsanghera@gmail.com>
2021-04-01 13:25:28 -0700

    Added Switch with Route paths and imports

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,24 @@
-
+import {Route, Switch} from 'react-router-dom'
+import HomePage from '../pages/Home';
+import MembersPage from '../pages/Members'
+import MemberPage from '../pages/Member'
+import AppRouter from './routers/AppRouter';
+import Navbar from '../components/Navbar';
+import './App.css';
 const AppRouter=()=>{
-  return()
+  return(
+    <div>
+      <Navbar />
+      <div className="container">
+      <Switch>
+        <Route path='/' exact component={HomePage}></Route>
+        <Route path='/members' exact component={MembersPage}></Route>
+        <Route path='/member/:memberId' exact component={MemberPage}></Route>
+
+      </Switch>
+
+      </div>
+    </div>
+  )
 }
 export default AppRouter;
