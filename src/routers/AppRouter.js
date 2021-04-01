f5eb8902ab467e6620b2ddf58fb490b792c81902
Pardeep Sanghera <pradeepsanghera@gmail.com>
2021-04-01 14:52:47 -0700

    imported AddMemberPage to AppRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,6 +2,7 @@ import {Route, Switch} from 'react-router-dom'
 import HomePage from '../pages/Home';
 import MembersPage from '../pages/Members'
 import MemberPage from '../pages/Member'
+import AddMemberPage from '../pages/AddMember';
 import Navbar from '../components/Navbar';
 
 const AppRouter=()=>{
@@ -13,6 +14,7 @@ const AppRouter=()=>{
         <Route path='/' exact component={HomePage}></Route>
         <Route path='/members' exact component={MembersPage}></Route>
         <Route path='/member/:memberId' exact component={MemberPage}></Route>
+        <Route path='/addmember' exact component={AddMemberPage}></Route>
 
       </Switch>
 
